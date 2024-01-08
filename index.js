3f31a8516a9e2cab3efd9db05e7230b3812ec226
mukul-bhalla <bhallamukul27@gmail.com>
2024-01-08 16:16:13 +0530

    session

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const Joi = require('joi');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const User = require('./models/user')
 const catchAsync = require('./utils/catchAsync')
@@ -33,9 +35,27 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
+const sessionConfig = {
+    secret: 'thisshouldbeasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 app.use('/user', users)
 
 app.get('/', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -23,28 +23,21 @@ const validateUser = (req, res, next) => {
     }
 }
 
+
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id)
+    if (!user) {
+        req.flash('error', "Cannot Find The User !");
+        return res.redirect('/')
+    }
     res.render('show', { user });
 }))
 
 router.post('/register', validateUser, catchAsync(async (req, res, next) => {
-    const userSchema = Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string()
-            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-        phone: Joi.number().required(),
-        password: Joi.string().required()
-    })
-    const { error } = userSchema.validate(req.body);
-
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
     const user = new User(req.body);
     await user.save();
+    req.flash('success', 'Successfully Created Your Account !');
     res.redirect(`/user/${user._id}`)
 
 }))
@@ -53,6 +46,10 @@ router.post('/register', validateUser, catchAsync(async (req, res, next) => {
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
+    if (!user) {
+        req.flash('error', "Cannot Find The User !");
+        return res.redirect('/')
+    }
     res.render('edit', { user });
 }))
 
@@ -60,12 +57,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateUser, catchAsync(async (req, res) => {
     const { id } = req.params;
     await User.findByIdAndUpdate(id, req.body);
+    req.flash('success', "Successfully Updated Account Details !")
     res.redirect(`/user/${id}`)
 }))
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await User.findByIdAndDelete(id)
+    req.flash('success', "Successfully Deleted Your Account !")
     res.redirect('/')
 }))
 
